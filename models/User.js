a9a1c90b1be84ccfe18874853b59c4fa1e357e81
lotus <daniel.younghwan.lee@gmail.com>
2022-06-24 12:33:23 -0400

    hashing password

--- models/User.js
+++ models/User.js
@@ -1,7 +1,9 @@
+const bcrypt = require('bcrypt');
+
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class User extends Model {}
+class User extends Model { }
 
 User.init(
     {
@@ -32,6 +34,16 @@ User.init(
         }
     },
     {
+        hooks: {
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData
+            },
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
         sequelize,
         timestamps: false,
         freezeTableName: true,
