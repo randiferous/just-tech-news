fd635eff5afabecb3fec5847f2e3426640bed92b
lotus <daniel.younghwan.lee@gmail.com>
2022-07-01 14:24:55 -0400

    cleaning up code

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -19,7 +20,7 @@ const sess = {
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
